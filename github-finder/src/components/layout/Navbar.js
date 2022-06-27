5d1fd64b6a64bec84a806e3e80ef922da68305d9
pat <31299967+pcarderdev@users.noreply.github.com>
2022-06-26 21:27:10 -0400

    Repos init
    Started work on repos in context/reducer. Next will be finishing Repo and RepoList

--- github-finder/src/components/layout/Navbar.js
+++ github-finder/src/components/layout/Navbar.js
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types'
 function Navbar({ title }) {
 
     return (
-        <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
+        <nav className="navbar mb-12 shadow-lg bg-neutral">
             <div className="container mx-auto">
                 <div className="flex-none px-2 mx-2">
                     <FaGithub className="inline pr-2 text-3xl" />
--- github-finder/src/context/github/GithubContext.js
+++ github-finder/src/context/github/GithubContext.js
@@ -10,6 +10,8 @@ export const GithubProvider = ({ children }) => {
     
     const initialState = {
         users: [],
+        user: {},
+        repos: [],
         loading: false,
     }
 
@@ -38,6 +40,47 @@ export const GithubProvider = ({ children }) => {
         })
     }
 
+    // Get single user
+    const getUser = async (login) => {
+        
+        setLoading()
+
+        const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`
+            }
+        })
+
+        if(response.status === 404) {
+            window.location = '/notfound'
+        } else {
+            const data = await response.json()
+        
+            dispatch({
+                type: 'GET_USER',
+                payload: data,
+            })
+        }
+    }
+
+    // Get user repos
+    const getUserRepos = async (login) => {
+        setLoading()
+
+        const reponse = await fetch(`${GITHUB_URL}/users/${login}/repos`, {
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`,
+            },
+        })
+
+        const data = await reponse.json()
+
+        dispatch({
+            type: 'GET_REPOS',
+            payload: data,
+        })
+    }
+
     const clearUsers = () => dispatch({
         type: 'CLEAR_USERS'
     })
@@ -51,8 +94,12 @@ export const GithubProvider = ({ children }) => {
         <GithubContext.Provider value={{
             users: state.users,
             loading: state.loading,
+            user: state.user,
+            repos: state.repos,
             searchUsers,
             clearUsers,
+            getUser,
+            getUserRepos,
         }}>
             {children}
         </GithubContext.Provider>
