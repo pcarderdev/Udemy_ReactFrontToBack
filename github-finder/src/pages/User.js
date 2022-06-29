9566cebc902a0fe11d43dbb35671ea410bc97fd7
pat <31299967+pcarderdev@users.noreply.github.com>
2022-06-29 17:12:01 -0400

    Github Finder proj finish

--- github-finder/src/pages/User.js
+++ github-finder/src/pages/User.js
@@ -5,16 +5,24 @@ import { useParams, Link } from 'react-router-dom'
 import { FaCodepen, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa'
 import Spinner from '../components/layout/Spinner'
 import RepoList from '../components/repos/RepoList'
+import { getUserAndRepos } from '../context/github/GithubActions'
 
 function User() {
 
-    const { getUser, user, loading } = useContext(GithubContext)
+    const { user, loading, repos, dispatch } = useContext(GithubContext)
 
     const params = useParams()
 
     useEffect(() => {
-        getUser(params.login)
-    }, [])
+        dispatch({type: 'SET_LOADING'})
+
+        const getUserData = async() => {
+            const userData = await getUserAndRepos(params.login)
+            dispatch({type: 'GET_USER_AND_REPOS', payload: userData})    
+        }
+
+        getUserData()
+    }, [dispatch, params.login])
 
     const {
         name,
@@ -152,8 +160,7 @@ function User() {
                         </div>
                     </div>
                 </div>
-
-                <RepoList />
+                <RepoList repos={repos}/>
             </div>
         </>
     )
