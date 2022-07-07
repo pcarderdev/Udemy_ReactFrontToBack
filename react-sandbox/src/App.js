6d536993282cad7cd3937a2e23f9024c2f950141
pat <31299967+pcarderdev@users.noreply.github.com>
2022-07-07 16:08:08 -0400

    Advanced Hooks pt 1

---
+++ react-sandbox/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import UseRefExample1 from './components/UseRefExample1'
+
+function App() {
+  return (
+    <div className="container mt-5">
+        <UseRefExample1 />
+    </div>
+  )
+}
+
+export default App
